bfd2cf612c3a76d64103497152ba930ba27ce272
Vitalik Yashchuk <114389609+Vitaliy-Yashchuk@users.noreply.github.com>
2024-11-10 21:36:37 +0200

    add car spawn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,29 +4,70 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject car;
+    public float timeToSpawn = 3f;
     private void Start()
     {
         StartCoroutine(BottomCars());
+        StartCoroutine(LeftCars());
+        StartCoroutine(RightCars());
+        StartCoroutine(UpCars());
     }
 
     IEnumerator BottomCars()
     {
         while (true)
         {
-           GameObject newObj = Instantiate(car, new Vector3(-1.59f, -0.001841247f, -24.97f), Quaternion.Euler(0,180,0)) as GameObject;
-            int rand = Random.Range(1, 4);
-            switch (rand)
-            {
-                case 1:
-                    newObj.GetComponent<CarController>().rightTurn = true;
-                    break;
-                case 2:
-                    newObj.GetComponent<CarController>().leftTurn = true;
-                    break;
-                case 3:
-                    break;
-            }
-            yield return new WaitForSeconds(1f);
+            SpawnCar(new Vector3(-1.59f, -0.001841247f, -24.97f),180f);
+            yield return new WaitForSeconds(timeToSpawn);
+        }
+    }
+
+    IEnumerator LeftCars()
+    {
+        while (true)
+        {
+            SpawnCar(new Vector3(-89.4f, -0.001841247f, 3.5f),270f);
+            yield return new WaitForSeconds(timeToSpawn);
+        }
+    } 
+    IEnumerator RightCars()
+    {
+        while (true)
+        {
+            SpawnCar(new Vector3(26.8f, -0.001841247f, 10.2f),90f);
+            yield return new WaitForSeconds(timeToSpawn);
+        }
+    }
+    IEnumerator UpCars()
+    {
+        while (true)
+        {
+            SpawnCar(new Vector3(-9f, -0.001841247f, 71.7f),0f);
+            yield return new WaitForSeconds(timeToSpawn);
+        }
+    }
+
+    private void SpawnCar(Vector3 pos, float rotationY, bool isMoveFromUp = false)
+    {
+        
+        GameObject newObj = Instantiate(car, pos, Quaternion.Euler(0,rotationY,0)) as GameObject;
+        int rand = Random.Range(1, 7);
+        switch (rand)
+        {
+            case 1:
+            case 2:
+                newObj.GetComponent<CarController>().rightTurn = true;
+                break;
+            case 3:
+            case 4:
+                newObj.GetComponent<CarController>().leftTurn = true;
+                if (isMoveFromUp)
+                {
+                    newObj.GetComponent<CarController>().moveFromUp = true;
+                }
+                break;
+            case 5:
+                break;
         }
     }
 }
